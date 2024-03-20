c219dcffb2351c7aa73f3315a70635bc1f8d2ca1
vtAndreola <dev.andreola@gmail.com>
2024-03-20 20:17:48 -0300

    feat: Create hero section

---
+++ src/app/hero-section/page.tsx
@@ -0,0 +1,28 @@
+const HeroSection = () => {
+  return (
+    <section className="py-14 bg-gradient-to-r h-full from-white to-gray-200">
+      <div className="container mx-auto text-center space-y-5">
+        <div className="text-4xl sm:text-6xl flex justify-center md:pb-3  font-bold md:px-20 text-gradient bg-gradient-to-r from-lime-400 to-lime-600 bg-clip-text text-transparent overflow-visible">
+          A simplicidade que você precisa, onde a tecnologia encontra a
+          praticidade
+        </div>
+        <p className="text-md md:text-xl md-10 bg-gradient-to-r to-gray-900 from-gray-400 bg-clip-text text-transparent font-bold">
+          Transformando suas necessidades em soluções tecnológicas eficientes.
+        </p>
+        <div className="flex gap-4 justify-center">
+          <button className="bg-gradient-to-r from-lime-500 to-lime-600 hover:from-lime-600 hover:to-lime-700 transition-colors duration-300 text-white px-10 py-4 rounded-md md:text-lg font-bold">
+            Começar
+          </button>
+          <button className="px-10 py-4 text-nowrap rounded-md md:text-lg font-bold border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground">
+            Saiba Mais
+          </button>
+        </div>
+        <video className="rounded-xl md:w-2/3 mx-auto" autoPlay muted loop>
+          <source src="/content/hero.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import HeroSection from "./hero-section/page";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 }
