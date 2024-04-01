27344296b52e2657867cc01a2821544d5599d59d
vtAndreola <dev.andreola@gmail.com>
2024-04-01 19:24:03 -0300

    style: Add intro animation

--- src/app/intro-section/page.tsx
+++ src/app/intro-section/page.tsx
@@ -1,16 +1,41 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 
+import { gsap } from "gsap/gsap-core";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
 const Intro = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".intro-side", {
+      opacity: 1,
+      duration: 2,
+      x: 0,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: ".intro-trigger",
+        start: "top 70%",
+        end: "bottom 20%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  }, []);
+
   return (
     <section className="py-14 bg-gradient-to-l h-full to-white from-gray-200">
-      <div className="lg:flex-row flex-col items-center flex justify-center max-w-6xl mx-auto p-5">
-        <video className="w-full lg:w-2/4 m-5 rounded-xl" autoPlay muted loop>
+      <div className="lg:flex-row flex-col items-center flex justify-center max-w-6xl mx-auto p-5 intro-trigger">
+        <video
+          className="w-full lg:w-2/4 m-5 rounded-xl intro-side opacity-0 -translate-x-4"
+          autoPlay
+          muted
+          loop
+        >
           <source src="/content/intro.mp4" type="video/mp4" />
         </video>
-        <div className="justify-center lg:p-5">
+        <div className="justify-center lg:p-5 intro-side opacity-0 translate-x-4">
           <h2
             className="
                 bg-gradient-to-l
