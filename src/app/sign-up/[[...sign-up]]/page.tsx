dbe78f0e34688d9291c937d11a35c896c7679ef3
vtAndreola <dev.andreola@gmail.com>
2024-04-02 09:50:29 -0300

    style: Add sign pages margin

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center mt-10">
+    <div className="flex items-center justify-center my-10">
       <SignUp />
     </div>
   );
